75f25d9aacd08ff515396a5d0298e32e7819ef7b
Ly1515 <A01655861@tec.mx>
2022-09-16 18:22:43 -0500

    Modify food movement and restrictions

--- videojuegos/juegoSnake/Snake_edit.py
+++ videojuegos/juegoSnake/Snake_edit.py
@@ -27,6 +27,10 @@ def inside(head):
     """Return True if head inside boundaries."""
     return -200 < head.x < 190 and -200 < head.y < 190
 
+def insidef(food):
+    """Return True if food inside boundaries."""
+    return -200 < food.x < 190 and -200 < food.y < 190   
+
 
 def move():
     """Move snake forward one segment."""
@@ -45,6 +49,22 @@ def move():
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
     else:
+    
+        movefood = randrange(0,3)
+        if movefood == 0:
+            food.x += 10
+        elif movefood == 1:
+            food.y -= 10
+        elif movefood == 2:
+            food.x -= 10
+        elif movefood == 3:
+            food.y += 10
+
+        if not insidef(food):
+            square(head.x, head.y, 9, 'red')
+            update()
+            return
+
         snake.pop(0)
 
     clear()
